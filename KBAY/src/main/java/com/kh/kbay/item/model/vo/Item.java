eb9216e25a999fa84f482a58e675a4d0284f253a
DESKTOP-QGKHJAO\user1 <her123418@gmail.com>
2026-03-19 09:20:07 +0900

    허승만(model.vo 정리)

--- KBAY/src/main/java/com/kh/kbay/item/model/vo/Item.java
+++ KBAY/src/main/java/com/kh/kbay/item/model/vo/Item.java
@@ -8,18 +8,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Item {
+	private int itemNo;
+	private int itemCdNo;
+	private int userNo;
+	private String itemTitle;
+	private int itemSize;
+	private String itemContent;
 	private int startPrice;
+	private char directBuy;
+	private int buyNowPrice;
+	private int currentPrice;
 	private Date startTime;
 	private Date endTime;
+	private char status;
 	private Date createAt;
-	private int itemNo;
-	private String status;
-	private String itemContent;
-	private String itemTitle;
-	private int itemCdNo;
-	private String CreateCountry;
-	private int BuyNowPrice;
-	private int sellerNo;
-	private int currentPrice;
-	private char directBuy;
+	private String cuntryNo;
+	private int deliveryNo;
 }
