8535f29dc29c3500df76133a139bfb1570547e61
DESKTOP-QGKHJAO\user1 <her123418@gmail.com>
2026-03-20 17:26:30 +0900

    허승만

--- KBAY/src/main/java/com/kh/kbay/common/PageInfo.java
+++ KBAY/src/main/java/com/kh/kbay/common/PageInfo.java
@@ -16,5 +16,21 @@ public class PageInfo {
     private int startPage;      // 페이징 바의 시작 번호
     private int endPage;        // 페이징 바의 끝 번호
 
+    private int limit;
+    private int offset;
+    private int totalCount;
 
+    public static PageInfo of(int page, int totalCount, int limit) {
+        int offset = (page - 1) * limit;
+        int maxPage = (int)Math.ceil((double)totalCount / limit);
+        return new PageInfo(page, limit, offset, totalCount, maxPage);
+    }
+
+	public PageInfo(int page, int limit, int offset, int totalCount, int maxPage) {
+		this.currentPage = page;
+		this.limit = limit;
+		this.offset = offset;
+		this.totalCount = totalCount;
+		this.maxPage = maxPage;
+	}
 }
